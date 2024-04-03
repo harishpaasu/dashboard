77f214bb966dabef052912720f24b020b7f5c13d
harish paasu <paasuharish6@gmail.com>
2024-04-03 23:27:09 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import{colorModeContext , useMode} from "./theme"
+import{ColorModeContext , useMode} from "./theme"
 import { CssBaseline , ThemeProvider } from '@mui/material'
+import { Routes , Route} from "react-router-dom"
 import Topbar from "./Scenes/global/Topbar"
+import DashBoard from "./Scenes/dashboard/Index"
+import Sidebar from "./Scenes/global/Sidebar"
 
 function App() {
   const[theme, colorMode]=useMode();
-  return (<colorModeContext.Provider value={colorMode}>
+  return (<ColorModeContext.Provider value={colorMode}>
     <ThemeProvider theme={theme}>
       <CssBaseline/>
     <div className='app'></div>
     <main className='content'>
       <Topbar/>
+      <Routes>
+        <Route path="/" element={<DashBoard/>}/>
+      </Routes>
     </main>
     </ThemeProvider>
-  </colorModeContext.Provider>
+  </ColorModeContext.Provider>
   )
 } 
 
