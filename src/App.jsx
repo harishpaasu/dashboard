dd1fa9006fd300121ca6e750aff57d109ecc03ea
harish paasu <paasuharish6@gmail.com>
2024-04-04 23:46:07 +0530

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,41 @@ import { Routes , Route} from "react-router-dom"
 import Topbar from "./Scenes/global/Topbar"
 import DashBoard from "./Scenes/dashboard/Index"
 import Sidebar from "./Scenes/global/Sidebar"
+// import Team from "./Scenes/team"
+// import Invoices from "./Scenes/invoices"
+// import Contact from "./Scenes/contact"
+// import Bar from "./Scenes/bar"
+// import Line from "./Scenes/line"
+// import Pie from "./Scenes/pie"
+// import FAQ from "./Scenes/faq"
+// import Geography from "./Scenes/geography"
+// import Calendar from "./Scenes/calendar"
+
+
 
 function App() {
   const[theme, colorMode]=useMode();
   return (<ColorModeContext.Provider value={colorMode}>
     <ThemeProvider theme={theme}>
       <CssBaseline/>
-    <div className='app'></div>
+    <div className='app'>
+      <Sidebar/>
     <main className='content'>
       <Topbar/>
       <Routes>
         <Route path="/" element={<DashBoard/>}/>
+        {/* <Route path="/team" element={<Team/>}/> */}
+        {/* <Route path="/contact" element={<Contact/>}/> */}
+        {/* <Route path="/bar" element={<Bar/>}/> */}
+        {/* <Route path="/line" element={<Line/>}/> */}
+        {/* <Route path="/invoices" element={<Invoices/>}/> */}
+        {/* <Route path="/pie" element={<Pie/>}/> */}
+        {/* <Route path="/faq" element={<FAQ/>}/> */}
+        {/* <Route path="/calendar" element={<Calendar/>}/> */}
+        {/* <Route path="/geography" element={<Geography/>}/> */}
       </Routes>
     </main>
+    </div>
     </ThemeProvider>
   </ColorModeContext.Provider>
   )
--- src/Scenes/global/Sidebar.jsx
+++ src/Scenes/global/Sidebar.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { useState } from 'react';
+import 'react-pro-sidebar/dist/css/styles.css';
+import {Box, IconButton,Typography, useTheme} from "@mui/material";
+import {Link} from 'react-router-dom';
+import {tokens} from '../../theme'
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined"
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined"
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined"
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptLongOutlined"
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined"
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined"
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined"
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined"
+import PieChartOutlinedOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined"
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined"
+import MenuOutlinedIcon from "@mui/icons-material/MenuBookOutlined"
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined"
+
+
 
 const Sidebar = () => {
+  const theme=useTheme();
+const colors =tokens(theme.palette.mode)
+const[isCollapsed,setIsCollapsed]= useState(false);
+const[selected,SetSelected] =useState('DashBoard');
   return (
     <div>Sidebar</div>
   )
